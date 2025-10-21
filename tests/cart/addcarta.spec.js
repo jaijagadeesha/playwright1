bc3fddc3b45ea208486d055ca5fe09ea63f3abdf
jaijagadeesha <jaijagadeesha@gmail.com>
2025-10-21 16:46:51 +0530

    Added the new logd

--- tests/cart/addcarta.spec.js
+++ tests/cart/addcarta.spec.js
@@ -5,4 +5,9 @@ test.beforeEach(async ({ page }) => {
     console.log("Checking the before test");
     
   await page.goto('https://freelance-learn-automation.vercel.app/login');
+
+  console.log("Checked after the test");
+  console.log("Checked after the test1");
+  console.log("Checked after the test1");
+
 });
