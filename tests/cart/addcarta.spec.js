c87c0118217c73f7d9630f4a3c9a06f71aa54ebd
jaijagadeesha <jaijagadeesha@gmail.com>
2025-10-21 16:28:01 +0530

    Updated rhe addcarta.spec.js file

--- tests/cart/addcarta.spec.js
+++ tests/cart/addcarta.spec.js
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
+import { log } from 'node:console';
 
 test.beforeEach(async ({ page }) => {
+    console.log("Checking the before test");
+    
   await page.goto('https://freelance-learn-automation.vercel.app/login');
 });
